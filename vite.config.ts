dd11c63574ff7b731ed387a7ae961a41c817b20c
danielduckworth <18698360+danielduckworth@users.noreply.github.com>
2022-11-07 09:25:46 +1100

    Manual storybook install

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+import vuetify from 'vite-plugin-vuetify'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vuetify()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    brotliSize: false, // Brotli unsupported in StackBlitz
+  },
+})
